eb7cd04bc4de8a51949b9cb2dcabf6bbdf2e9e55
Guilherme Moura Nascimento <gmoura.nas@gmail.com>
2013-09-01 11:42:54 -0300

    Estrutura do Plugin alterada para seguir os padrões jQuery

--- js/dropfy-2.0.js
+++ js/dropfy-2.0.js
@@ -1,81 +1,79 @@
-/*
-	<div<class="box-drop">
-		<a href="" class="open-drop">Selecione ...</a>
-		<div class="box-drop-extended aberto" style="display: block;">
-			<div class="drop-bg-topo"></div>
-			<ul rel="cidade-extended" class="drop-extended">
-				<li class="odd"><a rel="SÃƒÂ£o Paulo" href="#">SÃƒÂ£o Paulo</a></li>
-				<li class="even"><a rel="Rio de Janeiro" href="#">Rio de Janeiro</a></li>
-			</ul>
-			<div class="drop-bg-bottom"></div>
-		</div>
-	</div>
+/*!
+* jQuery dropfy
+* Author: iG Team
 */
+
 (function($){
-	/* Event Handlers */
-	var eventHandlers = {
-		changeValue :  function(e){
-			e.preventDefault();
-			var _this  		= $(this),
-				_value 		= _this.attr("data-value"),
-				_origem		= _this.closest('ul').attr('data-value'),
-				_selected 	= _this.closest('.box-drop').find('a.open-drop'),
-				_selectItem = $("select[name="+_origem+"]");
-			
-				/* 
-					Habilitando evento de click para o box-drop 
-				*/
-				_selected.html(_this.html()).trigger("click");
-				_selectItem.val(_value);
-				/* 
-					O bind para essa função passa o parametro options (recebibo como e.data.opt) e cria o evento customizado
-				*/
-				e.data.opt.onChange.call(undefined,_value);
-				
-			return false
-		},
-		showOptions :  function(e){
-			e.preventDefault();
-			var _this 	= $(this),
-				_parent = _this.parent(),
-				_classe = _parent.find('.box-drop-extended').parent().hasClass('box-drop fechado');
-			/* 
-				Se existir a classe 'box-drop fechado' exibimos o conteúdo e trocamos a classe para 'box-drop aberto' e vice-versa
-			*/
-			if(_classe){
-				_parent.css('z-index','999').find(".box-drop-extended")
-					.show()
-					.parent()
-					.removeClass("fechado")
-					.addClass("aberto");
-				_parent.find("li a:first").focus();
-			}
-			else{
-				_parent.css('z-index','1').find(".box-drop-extended")
-					.hide()
-					.parent()
-					.removeClass("aberto")
-					.addClass("fechado");
-			}
-			return false
-		}
-	}
 
 	$.fn.dropfy = function (options){
-		this.each(function(i, element){
-			var _combobox  	   	 = $(this).hide(),
-				_comboTheme 	 = $(this).attr('class'),
-				_itemClass 	   	 = '',
-				_selected  	   	 = $(this).find("option:selected").text(),
-				_estrutura 	   	 = $('<ul class="drop-extended"></ul>'),
-				_itensBox  		 = $('<div class="box-drop-extended"></div>')
-										.append('<div class="drop-bg-topo"></div>')
-										.append(_estrutura)
-										.append('<div class="drop-bg-bottom"></div>')
-										.hide(),
-				_itemSelecionado = $('<div class="'+ _comboTheme +' box-drop fechado"></div>')
-										.append('<a class="open-drop" href="#" id="currentItem">'+_selected+'</a>')
-										.append(_itensBox);
+		return this.each(function(){
+
+		var 
+
+				_combobox  	   		= $(this).hide(),
+				_comboTheme 	 		= $(this).attr('class'),
+				_itemClass 	   		= '',
+				_selected  	   		= $(this).find("option:selected").text(),
+				_estrutura 	   		= $('<ul class="drop-extended"></ul>'),
+				_itensBox  		 		= $('<div class="box-drop-extended"></div>')
+															.append('<div class="drop-bg-topo"></div>')
+															.append(_estrutura)
+															.append('<div class="drop-bg-bottom"></div>')
+															.hide(),
+				_itemSelecionado 	= $('<div class="'+ _comboTheme +' box-drop fechado"></div>')
+															.append('<a class="open-drop" href="#" id="currentItem">'+_selected+'</a>')
+															.append(_itensBox),
+
+				/* Event Handlers */
+				eventHandlers = {
+					changeValue :  function(e){
+						e.preventDefault();
+						var 
+							_this  			= $(this),
+							_value 			= _this.attr("data-value"),
+							_origem			= _this.closest('ul').attr('data-value'),
+							_selected 	= _this.closest('.box-drop').find('a.open-drop'),
+							_selectItem = $("select[name="+_origem+"]");
+						
+							/* 
+								Habilitando evento de click para o box-drop 
+							*/
+							_selected.html(_this.html()).trigger("click");
+							_selectItem.val(_value);
+							/* 
+								O bind para essa função passa o parametro options (recebibo como e.data.opt) e cria o evento customizado
+							*/
+							e.data.opt.onChange.call(undefined,_value);
+							
+						return false
+					},
+					showOptions :  function(e){
+						e.preventDefault();
+						var 
+							_this 	= $(this),
+							_parent = _this.parent(),
+							_classe = _parent.find('.box-drop-extended').parent().hasClass('box-drop fechado');
+						/* 
+							Se existir a classe 'box-drop fechado' exibimos o conteúdo e trocamos a classe para 'box-drop aberto' e vice-versa
+						*/
+						if(_classe){
+							_parent.css('z-index','999').find(".box-drop-extended")
+								.show()
+								.parent()
+								.removeClass("fechado")
+								.addClass("aberto");
+							_parent.find("li a:first").focus();
+						}
+						else{
+							_parent.css('z-index','1').find(".box-drop-extended")
+								.hide()
+								.parent()
+								.removeClass("aberto")
+								.addClass("fechado");
+						}
+						return false
+					}
+				}
 
 			/* 
 				Nessa função você resgata a quantidade de options e insere na estrutura nova
@@ -88,12 +86,10 @@
 				indice 	 == 1 ? _selected  = __value : '';
 				
 				_estrutura.append('<li class="'+_itemClass+'">'+
-								  	'<a href="#" data-value="'+__value+'">'+
-								  		$(value).html()+
-								  	'</a>'+ 
-								  '</li>');
-
-				// console.log(  $(value).attr('selected' )
+											  	'<a href="#" data-value="'+__value+'">'+
+											  		$(value).html()+
+											  	'</a>'+ 
+											  '</li>');
 
 			});					
 
@@ -111,6 +107,6 @@
 				Insere a estrutura do select customizado antes do Select criado no HTML 
 			*/
 			_combobox.before(_itemSelecionado);
-		})
+		});
 	}
 })(jQuery)
